0988aa0a38eab8c1006e52c63278b79309eaa808
Tresor-Koffi <tresor.koffi3@gmail.com>
2026-05-24 20:41:25 +0200

    fix api url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
-
+const api = axios.create({ baseURL: process.env.REACT_APP_API_URL + '/api' || '/api' });
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('mvm_token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
